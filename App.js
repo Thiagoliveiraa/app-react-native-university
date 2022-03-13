6a021df239c9136799e3efb39af1b632c8cf90a9
Thiago Oliveira <thiagooliveiraaa@hotmail.com>
2022-03-13 18:02:18 -0300

    app with expo

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("The button was clicked.");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World!</Text>
+      <Text>I'm Batman</Text>
+      <Text>
+        Two years of stalking the streets as the Batman, striking fear into the
+        hearts of criminals, has led Bruce Wayne deep into the shadows of Gotham
+        City. With only a few trusted allies—Alfred Pennyworth, Lt. James
+        Gordon—amongst the city corrupt network of officials and high-profile
+        figures, the lone vigilante has established himself as the sole
+        embodiment of vengeance amongst his fellow citizens.
+      </Text>
+      <Button title="Click on" onPress={handlePress} color="green" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "200",
   },
 });
